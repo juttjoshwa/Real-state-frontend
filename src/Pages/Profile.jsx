cbfe4abede95c70b54e3b043c49a57df7c77140e
juttjoshwa <juttjoshwa@example.com>
2024-01-03 03:57:24 +0500

    Profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -11,7 +11,45 @@ const Profile = () => {
     }
   }, []);
 
-  return <h1>Profile</h1>;
+  return (
+    <div className="p-1 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-3">
+        <img
+          src={currentUser.avatar}
+          id="username"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+          alt="Profile"
+        />
+        <input
+          id="email"
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase  hover:90% disabled:80%">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-3">
+        <span className="text-rose-700 cursor-pointer capitalize">
+          Delete account
+        </span>
+        <span className="text-red-700 cursor-pointer capitalize">Sign out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
